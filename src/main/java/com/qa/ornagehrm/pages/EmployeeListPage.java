bf478165ff010e88a1bc1510a0b1c851d5f3bbf0
mahendra319 <mahendra319@gmail.com>
2020-12-15 17:07:45 -0600

    this is OrangeHRM project first commit

---
+++ src/main/java/com/qa/ornagehrm/pages/EmployeeListPage.java
@@ -0,0 +1,28 @@
+package com.qa.ornagehrm.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.qa.orangehrm.utils.ElementUtil;
+
+public class EmployeeListPage {
+	
+	private WebDriver driver;
+	ElementUtil elementUtil;
+	
+	//constructor
+	
+	public EmployeeListPage(WebDriver driver) {
+		elementUtil = new ElementUtil(driver);
+		this.driver = driver;
+	}
+
+	//By locators
+	By header = By.tagName("h1");
+	
+	
+	
+	
+	
+	
+}
